0d3c8470999ab19f695c4aa9a61c86fb4a69b7c4
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-02-17 19:58:22 -0500

    feat: First commit

---
+++ QuotesOfBreakingBad/QuotesOfBreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesOfBreakingBadApp.swift
+//  QuotesOfBreakingBad
+//
+//  Created by Andersson on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct QuotesOfBreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
