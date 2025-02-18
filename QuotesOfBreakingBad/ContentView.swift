7893abea91f3a89f0046ca97946b460842071378
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-02-17 20:28:50 -0500

    feat: Add project configuration and tabview

--- QuotesOfBreakingBad/ContentView.swift
+++ QuotesOfBreakingBad/ContentView.swift
@@ -4,18 +4,23 @@
 //
 //  Created by Andersson on 17/02/25.
 //
+// https://breaking-bad-api-six.vercel.app/api
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Breaking Bad", systemImage: "flask") {
+                Text("Breaking Bad View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            Tab("Better Call Saul", systemImage: "candybarphone") {
+                Text("Better Call Saul View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
